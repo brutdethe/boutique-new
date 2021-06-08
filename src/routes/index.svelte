efd667d9855ee50990c1821df4553ddf1970aa3a
pntbr <pntbr@pm.me>
2021-06-08 13:07:40 +0200

    adds content for teapot product

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,33 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+	<title>Théière 西施-XīShī du 坭興-NíXìng</title>
+</svelte:head>
+
+<article>
+	<aside>
+		<img src="theiere_2-a.jpg" alt="" class="src" />
+	</aside>
+	<section>
+		<hgroup>
+			<h1>西施-XīShī du 坭興-NíXìng</h1>
+			<h2>Théière</h2>
+		</hgroup>
+		<p>
+			Les poteries de NíXìng sont uniques car elles obtiennent une coloration complexe et tachetée
+			pendant la cuisson. Elles sont fabriquées en mélangeant l'argile des deux rives de la rivière
+			欽江-QīnJiāng. La couleur change dans le four en évoluant du brun bronze au gris-bleu, rendant
+			chaque pièce unique. Elles conviennent particulièrement bien au 普洱生茶-Pǔ’ěr ShēngChá, aux
+			烏龍-WūLóng et aux 紅茶-HóngChá (rouge)..
+		</p>
+		<dl>
+			<dd>Capacité :</dd>
+			<dt>180ml</dt>
+			<dd>Poids :</dd>
+			<dt>300g</dt>
+			<dd>Dimensions :</dd>
+			<dt>lg: 11.8cm x ht: 7.2cm</dt>
+			<dd>Stock :</dd>
+			<dt>il reste 1 article</dt>
+		</dl>
+		<span itemprop="price">169.00 €</span>
+	</section>
+</article>
