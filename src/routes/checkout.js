c90c68bf829d56bf9966884546f26b37fecba677
pntbr <pntbr@pm.me>
2021-06-17 20:16:11 +0200

    creates a stripe checkout session

--- src/routes/checkout.js
+++ src/routes/checkout.js
@@ -1,5 +1,26 @@
+import Stripe from 'stripe'
+
 export async function get() {
+    const stripe = new Stripe('insert your stripe secret key here!')
+
+    const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        line_items: [{
+            price_data: {
+                currency: 'usd',
+                product_data: {
+                    name: 'Dégaine'
+                },
+                unit_amount: 2000,
+            },
+            quantity: 1,
+        }],
+        mode: 'payment',
+        success_url: 'http://localhost/success.html',
+        cancel_url: 'http://localhost/cancel.html',
+    })
+
     return {
-        "body": "Yeah ! This is where stuff should be !"
+        "body": session.id
     }
 }
